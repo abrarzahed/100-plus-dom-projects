18fde615b276e7cbef0a048060905b9622abfb59
abrarzahed <abrarzahed6986@gmail.com>
2022-03-03 00:14:34 +0600

    funtional approch

--- project_1/app.js
+++ project_1/app.js
@@ -1,8 +1,22 @@
-// selection of dom
 const root = document.getElementById("root");
 const changer = document.getElementById("changeBtn");
 
-// event listeners
-changer.addEventListener("click", () => {
-  root.style.background = "crimson";
+window.onload = () => {
+  generateBg();
+  assignBg();
+};
+
+function generateBg() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function assignBg() {
+  root.style.background = generateBg();
+}
+
+changer.addEventListener("click", function () {
+  root.style.background = generateBg();
 });
