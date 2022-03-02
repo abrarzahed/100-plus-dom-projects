c403ea37d80e001acdd25c173b9816aa7056951a
abrarzahed <abrarzahed6986@gmail.com>
2022-03-02 23:35:39 +0600

    completed first project

---
+++ project_1/app.js
@@ -0,0 +1,8 @@
+// selection of dom
+const root = document.getElementById("root");
+const changer = document.getElementById("changeBtn");
+
+// event listeners
+changer.addEventListener("click", () => {
+  root.style.background = "crimson";
+});
