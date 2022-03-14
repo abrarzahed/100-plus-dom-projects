4dd4a2a16c7fd71b682f832a6a16c0e67abe18e4
abrarzahed <abrarzahed6986@gmail.com>
2022-03-14 22:25:28 +0600

    image slider

---
+++ image-slider/app.js
@@ -0,0 +1,45 @@
+const sliderImages = document.querySelectorAll(".slide");
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+let current = 0;
+
+// clear slides
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+// initialize slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+//   show prev
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+//   show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// left arrow click
+arrowLeft.addEventListener("click", function () {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// right arrow click
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+startSlide();
